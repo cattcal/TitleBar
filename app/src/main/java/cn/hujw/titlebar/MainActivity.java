6e67ac97580fcf133d84e06010622ec09b4a25df
hujiangwei <hujw_android@163.com>
2019-08-04 18:26:46 +0800

    打造Android通用的标题栏

--- app/src/main/java/cn/hujw/titlebar/MainActivity.java
+++ app/src/main/java/cn/hujw/titlebar/MainActivity.java
@@ -2,12 +2,35 @@ package cn.hujw.titlebar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import cn.hujw.titlebar.library.OnTitleBarListener;
+import cn.hujw.titlebar.library.TitleBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TitleBar mTitleBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTitleBar = findViewById(R.id.mTitleBar);
+        mTitleBar.setOnTitleBarListener(new OnTitleBarListener() {
+            @Override
+            public void onLeftClick(View v) {
+
+            }
+
+            @Override
+            public void onTitleClick(View v) {
+
+            }
+
+            @Override
+            public void onRightClick(View v) {
+
+            }
+        });
     }
 }
